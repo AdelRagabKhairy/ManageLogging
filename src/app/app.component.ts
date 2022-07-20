333183c50c488c65bb624db2c8fa29f61b734d81
Adel Ragab <adelragab197@gmail.com>
2022-07-20 11:18:49 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './Models/User';
+import { ManageaccountService } from './Services/manageaccount.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MangeLogging.UI';
+ 
+ 
+  Useres: User[] = [];
+  UserToEdit?: User;
+
+  constructor(private UserService: ManageaccountService) {}
+
+  ngOnInit(): void {
+    this.UserService
+      .getUseres()
+      .subscribe((result: User[]) => (this.Useres = result));
+  }
+
+  updateUserList(Useres: User[]) {
+    this.Useres = Useres;
+  }
+
+  initNewUser() {
+    this.UserToEdit = new User();
+  }
+
+  editUser(User: User) {
+    this.UserToEdit = User;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ManageUsersComponent } from './Components/manage-users/manage-users.component';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManageUsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
